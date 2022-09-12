705dd4cf23353946287ed536c6c33c3ef69bfd22
kalluriyogeswararao <kalluriyogesh9@gmail.com>
2022-09-12 06:23:11 +0000

    product-item-adding

---
+++ src/components/ProductItemDetails/index.js
@@ -0,0 +1,90 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class ProductItemDetails extends Component {
+  state = {apiStatus: apiStatusConstants.initial}
+
+  componentDidMount() {
+    this.getProductItemDetails()
+  }
+
+  productsNotFoundView = () =>
+    this.setState({apiStatus: apiStatusConstants.failure})
+
+  getProductItemDetails = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    const url = `https://apis.ccbp.in/products/:${id}`
+
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+
+    if (response.ok) {
+      return null
+    }
+    return this.productsNotFoundView()
+  }
+
+  onClickContinueShopping = () => {
+    const {history} = this.props
+    history.replace('/products')
+  }
+
+  onProductNotFound = () => (
+    <div className="notfound-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+        alt="error view"
+        className="error-view-product"
+      />
+      <h1 className="not-found">Product Not Found</h1>
+      <button
+        type="button"
+        className="shopping-btn"
+        onClick={this.onClickContinueShopping}
+      >
+        Continue Shopping
+      </button>
+    </div>
+  )
+
+  onDisplayDetails = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.inProgress:
+        return this.inProgressData()
+      case apiStatusConstants.failure:
+        return this.onProductNotFound()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return <div>{this.onDisplayDetails()}</div>
+  }
+}
+
+export default ProductItemDetails
